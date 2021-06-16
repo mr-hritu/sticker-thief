5eaf91725c26ef44e71327b779dc962c2250e31e
zeroone2numeral2 <numeralzeroone@gmail.com>
2021-06-16 18:36:40 +0200

    PicklePersistenceWorkaround: override proxy method too
    Also update docstrings and re-locate try/excepts. Basically a PR draft

--- bot/pickle_persistence_workaround.py
+++ bot/pickle_persistence_workaround.py
@@ -7,6 +7,22 @@ from telegram.ext import PicklePersistence
 
 
 class PicklePersistenceWorkaround(PicklePersistence):
+    def insert_bot(self, obj: object) -> object:
+        """
+        Replaces all instances of :attr:`REPLACED_BOT` that occur within the passed object with
+        :attr:`bot`. Currently, this handles objects of type ``list``, ``tuple``, ``set``,
+        ``frozenset``, ``dict``, ``defaultdict`` and objects that have a ``__dict__`` or
+        ``__slots__`` attribute, excluding classes and objects that can't be copied with
+        ``copy.copy``, or that don't allow to change their attributes with ``__setattr__``.
+
+        Args:
+            obj (:obj:`object`): The object
+
+        Returns:
+            :obj:`obj`: Copy of the object with Bot instances inserted.
+        """
+        return self._insert_bot(obj, {})
+
     def _insert_bot(self, obj: object, memo: Dict[int, object]) -> object:  # pylint: disable=R0911
         obj_id = id(obj)
         if obj_id in memo:
@@ -62,27 +78,58 @@ class PicklePersistenceWorkaround(PicklePersistence):
                 new_obj[self._insert_bot(k, memo)] = self._insert_bot(val, memo)
             memo[obj_id] = new_obj
             return new_obj
-        if hasattr(obj, '__dict__'):
-            for attr_name, attr in new_obj.__dict__.items():
-                try:
+        try:
+            if hasattr(obj, '__dict__'):
+                for attr_name, attr in new_obj.__dict__.items():
                     setattr(new_obj, attr_name, self._insert_bot(attr, memo))
-                except Exception:
-                    memo[obj_id] = obj
-                    return obj
 
-            memo[obj_id] = new_obj
-            return new_obj
-        if hasattr(obj, '__slots__'):
-            for attr_name in obj.__slots__:
-                setattr(
-                    new_obj,
-                    attr_name,
-                    self._insert_bot(self._insert_bot(getattr(new_obj, attr_name), memo), memo),
-                )
-            memo[obj_id] = new_obj
-            return new_obj
+                memo[obj_id] = new_obj
+                return new_obj
+        except Exception:
+            warnings.warn(
+                'BasePersistence.insert_bot does not handle objects that don\'t allow to change their attributes '
+                'with setattr(). See the docs of BasePersistence.insert_bot for more information.',
+                RuntimeWarning,
+            )
+            memo[obj_id] = obj
+            return obj
+        try:
+            if hasattr(obj, '__slots__'):
+                for attr_name in obj.__slots__:
+                    setattr(
+                        new_obj,
+                        attr_name,
+                        self._insert_bot(self._insert_bot(getattr(new_obj, attr_name), memo), memo),
+                    )
+                memo[obj_id] = new_obj
+                return new_obj
+        except Exception:
+            warnings.warn(
+                'BasePersistence.insert_bot does not handle objects that don\'t allow to change their attributes '
+                'with setattr(). See the docs of BasePersistence.insert_bot for more information.',
+                RuntimeWarning,
+            )
+            memo[obj_id] = obj
+            return obj
 
         return obj
+
+    @classmethod
+    def replace_bot(cls, obj: object) -> object:
+        """
+        Replaces all instances of :class:`telegram.Bot` that occur within the passed object with
+        :attr:`REPLACED_BOT`. Currently, this handles objects of type ``list``, ``tuple``, ``set``,
+        ``frozenset``, ``dict``, ``defaultdict`` and objects that have a ``__dict__`` or
+        ``__slots__`` attribute, excluding classes and objects that can't be copied with
+        ``copy.copy``, or that don't allow to change their attributes with ``__setattr__``.
+
+        Args:
+            obj (:obj:`object`): The object
+
+        Returns:
+            :obj:`obj`: Copy of the object with Bot instances replaced.
+        """
+        return cls._replace_bot(obj, {})
 
     @classmethod
     def _replace_bot(cls, obj: object, memo: Dict[int, object]) -> object:  # pylint: disable=R0911
@@ -138,24 +185,38 @@ class PicklePersistenceWorkaround(PicklePersistence):
                 new_obj[cls._replace_bot(k, memo)] = cls._replace_bot(val, memo)
             memo[obj_id] = new_obj
             return new_obj
-        if hasattr(obj, '__dict__'):
-            for attr_name, attr in new_obj.__dict__.items():
-                try:
+        try:
+            if hasattr(obj, '__dict__'):
+                for attr_name, attr in new_obj.__dict__.items():
                     setattr(new_obj, attr_name, cls._replace_bot(attr, memo))
-                except Exception:
-                    memo[obj_id] = obj
-                    return obj
 
-            memo[obj_id] = new_obj
-            return new_obj
-        if hasattr(obj, '__slots__'):
-            for attr_name in new_obj.__slots__:
-                setattr(
-                    new_obj,
-                    attr_name,
-                    cls._replace_bot(cls._replace_bot(getattr(new_obj, attr_name), memo), memo),
-                )
-            memo[obj_id] = new_obj
-            return new_obj
+                memo[obj_id] = new_obj
+                return new_obj
+        except Exception:
+            warnings.warn(
+                'BasePersistence.insert_bot does not handle objects that don\'t allow to change their attributes '
+                'with setattr(). See the docs of BasePersistence.insert_bot for more information.',
+                RuntimeWarning,
+            )
+            memo[obj_id] = obj
+            return obj
+        try:
+            if hasattr(obj, '__slots__'):
+                for attr_name in new_obj.__slots__:
+                    setattr(
+                        new_obj,
+                        attr_name,
+                        cls._replace_bot(cls._replace_bot(getattr(new_obj, attr_name), memo), memo),
+                    )
+                memo[obj_id] = new_obj
+                return new_obj
+        except Exception:
+            warnings.warn(
+                'BasePersistence.insert_bot does not handle objects that don\'t allow to change their attributes '
+                'with setattr(). See the docs of BasePersistence.insert_bot for more information.',
+                RuntimeWarning,
+            )
+            memo[obj_id] = obj
+            return obj
 
         return obj
