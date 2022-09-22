e361d8d5ffbd86d64ab8f6bbcfefb55682277187
zeroone2numeral2 <numeralzeroone@gmail.com>
2022-09-22 11:47:56 +0200

    /toemoji: fixed string

--- bot/strings.py
+++ bot/strings.py
@@ -283,8 +283,8 @@ class Strings:
 
     TO_FILE_MIME_TYPE = "mime-type: {}"
 
-    TO_EMOJI_WAITING_STICKER = "Send me a static/video sticker, " \
-                                      "I will send you back a file you can use as custom emoji"
+    TO_EMOJI_WAITING_STICKER = "Send me a static sticker, " \
+                               "I will send you back a file you can use as custom emoji"
 
     TO_EMOJI_UNEXPECTED_MESSAGE = "Please send me a <b>static</b> stickers, or /cancel"
 
